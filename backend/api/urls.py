55e00fe78b88e4388f2f487f5d40cf1f2574576f
Faiyaad Hossain <fyd41498@gmail.com>
2024-03-21 23:34:27 -0400

    added backend for user registration

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,15 @@
 from api import views
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
-    path("", views.getRouters,name="getRoutes"),
-    path("login/")
+    path("", views.getRoutes,name="getRoutes"),
+    # path("login/"),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.getUserProfile, name='getUserProfile'),
+    path('users/', views.getUsers, name='getUsers'),
+    path('users/register/', views.registerUser, name='register'),
+    path('activate/<uidb64>/<token>', views.ActivateAccountView.as_view(), name='activate')
 ]
