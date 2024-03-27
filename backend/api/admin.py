faffbca74eba3cd9716cfb5c743100d99b81842f
vorrapard <vk2584@nyu.edu>
2024-03-27 14:54:51 -0400

    Add UserProfile, RoomListing, Message, Favorite database models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile, RoommateListing, Message, Favorite
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(RoommateListing)
+admin.site.register(Message)
+admin.site.register(Favorite)
