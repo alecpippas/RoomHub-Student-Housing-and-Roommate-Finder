a8af7dd05777e02dd00a41e3cc3e37c325338b9f
vorrapard <vk2584@nyu.edu>
2024-04-10 16:51:09 -0400

    listing page and create listing page

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('users/profile/', getUserProfile, name='getUserProfile'),
     path('users/', getUsers, name='getUsers'),
     path('users/register/', registerUser, name='register'),
-    path('api/listings/create', CreateListingView.as_view(), name='create-listing'),
-    path('api/listings/', ListListingsView.as_view(), name='list-listings'),
+    path('listings/create/', CreateListingView.as_view(), name='create-listing'),
+    path('listings/', ListListingsView.as_view(), name='list-listings'),
     path('users/profile/display/<str:username>/', views.getProfile, name='getProfile'),
     path('users/profile/update/', views.editProfile, name='update'),
 ]
