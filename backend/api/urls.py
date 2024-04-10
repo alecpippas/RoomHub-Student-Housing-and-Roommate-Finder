7f105b0f45df3a75172e1c4fd224f6b5bc39ec0d
Faiyaad Hossain <fyd41498@gmail.com>
2024-04-10 12:47:31 -0400

    fixed syntax error

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('users/', getUsers, name='getUsers'),
     path('users/register/', registerUser, name='register'),
     path('api/listings/create', CreateListingView.as_view(), name='create-listing'),
-    path('api/listings/', ListListingsView.as_view(), name='list-listings')
+    path('api/listings/', ListListingsView.as_view(), name='list-listings'),
     path('users/profile/display/<str:username>/', views.getProfile, name='getProfile'),
     path('users/profile/update/', views.editProfile, name='update'),
 ]
